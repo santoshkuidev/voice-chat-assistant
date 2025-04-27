ec9cdd6a245b947d9e0815b07c801c468c00dec4
user <user@users-MacBook-Pro.local>
2025-04-26 20:28:40 -0700

    Add default cookie

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main';
 
+// Set mode cookie to 'profile' on initialization
+if (typeof document !== 'undefined') {
+  document.cookie = 'mode=profile; path=/';
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
